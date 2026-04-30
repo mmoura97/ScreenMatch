c183b87d637743082908713cb9ace4e1a4f16063
mmoura97 <matheus.mourd30@gmail.com>
2026-04-30 15:53:08 -0300

    feat: adiciona primeiros estudos em Java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlao = true;
+        double notaDoFilme = 8.1;
+
+
+    }
+}
+
