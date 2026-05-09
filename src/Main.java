b23c6ba4f45431fc134b564bb212a4e7bbf1e51e
mmoura97 <matheus.mourd30@gmail.com>
2026-05-08 23:29:17 -0300

    chore: organiza estrutura do projeto

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,20 @@ public class Main {
         boolean incluidoNoPlao = true;
         double notaDoFilme = 8.1;
 
+        double media = (9.9 + 6.3 + 8.0) / 3;
+        System.out.println("Nota do filme: " + media);
+
+        String sinopse;
+        sinopse = """
+                Filme: Top Gun
+                Filme de aventura com ator foda dos anos 80
+                Muito bom!
+                Ano de Lancamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) media / 2;
+        System.out.println(classificacao);
 
     }
 }
