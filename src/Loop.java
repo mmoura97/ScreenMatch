8ce9ddcc93456abddc1bb26e175e6a4eb002562f
mmoura97 <matheus.mourd30@gmail.com>
2026-05-10 20:40:48 -0300

    feat: adiciona loop for para leitura de avaliações

---
+++ src/Loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        double mediaavaliacao = 0;
+        double notaDoFilme = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Qual a sua avaliação oara o filme?");
+            notaDoFilme = leitura.nextDouble();
+            mediaavaliacao += notaDoFilme;
+        }
+
+        System.out.println("Media de Avaliações " + mediaavaliacao);
+
+    }
+}
